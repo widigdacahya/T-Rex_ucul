25fed0b7d3813571f0c577eb4ded6896b81bdce4
Allam Taju Sarof <allamtaju4@gmail.com>
2020-12-26 15:39:00 +0700

    12

--- JavaGameTutorial/src/id/ac/its/trexucul/game/window/Handler.java
+++ JavaGameTutorial/src/id/ac/its/trexucul/game/window/Handler.java
@@ -39,28 +39,5 @@ public LinkedList<GameObject> object = new LinkedList<GameObject>();
 		this.object.remove(object);
 	}
 	
-	public void createLevel() {
-		//handler.addObject(new Test(100,100,ObjectId.Test));
-				for(int xx=0; xx<Game.WIDTH+32; xx+=32) {
-					addObject(new Block(xx, Game.HEIGHT-32, ObjectId.Block));
-				}
-				
-				for(int right = Game.HEIGHT-64; right > -32; right -=32) {
-					addObject(new Block(Game.WIDTH-32,right,ObjectId.Block));
-				}
-				
-				for(int left = Game.HEIGHT-32; left > -32; left -= 32) {
-					addObject(new Block(0, left, ObjectId.Block));
-				}
-				
-				for(int center = 400; center <= 496; center += 32) {
-					addObject(new Block(center, 390,ObjectId.Block));
-				}
-				
-				for(int floatingBlock = 120; floatingBlock <= 216; floatingBlock += 32) {
-					addObject(new Block((floatingBlock), 420, ObjectId.Block));
-				}
-				
-	}
 	
 }
